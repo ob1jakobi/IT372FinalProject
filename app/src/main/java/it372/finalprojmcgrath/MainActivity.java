80dc480283504405063188917778d3e04c5e6c81
Jake McGrath <thejakezilla80@gmail.com>
2022-06-05 10:27:35 -0500

    Completely redone starting activity to now provide the option of looking at past reviews or make a new review.  Removed all rating entries from main page and moved it into new activity_rating.xml.  Created a new activity for reviewing all journal entries currently entered.

--- app/src/main/java/it372/finalprojmcgrath/MainActivity.java
+++ app/src/main/java/it372/finalprojmcgrath/MainActivity.java
@@ -1,14 +1,46 @@
 package it372.finalprojmcgrath;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+/**
+ * Name:    Jake McGrath
+ * Proj:    Final Project
+ * Due:     08 June 2022
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnMyReviews;
+    private Button btnNewRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize the buttons
+        btnMyReviews = findViewById(R.id.btn_my_ratings);
+        btnNewRating = findViewById(R.id.btn_new_rating);
+
+        // create functionality for button leading to all ratings entered activity
+        btnMyReviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyRatingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // create functionality for button leading to new rating activity
+        btnNewRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RatingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/it372/finalprojmcgrath/MyRatingsActivity.java
@@ -0,0 +1,14 @@
+package it372.finalprojmcgrath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyRatingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ratings);
+    }
+}
